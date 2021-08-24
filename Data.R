c15be922be26619d963e99c7ca7fdc19e02d9202
Corbyn Gilmore <Gilmorec1@livemail.uthscsa.edu>
2021-08-24 12:02:27 -0500

    Addded comments

--- Data.R
+++ Data.R
@@ -1,9 +1,14 @@
+#' Loading R libraries
 library(ggplot2)
 library(dplyr)
 library(GGally)
+#' Create data set
 dat0 <- survival::veteran;
+#' base R scatterplot matrix
 plot(dat0)
+#' show first 10 lines of dat0
 head(dat0)
-pairs(dat0)
+#' gg plot scatterplot matrix
 ggpairs(dat0)
+#' how to assign value to object and variable
 foo <- 3
