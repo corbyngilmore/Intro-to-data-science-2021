5b7c46a0d235418cfe42401b0af9b3acbcc8d727
Corbyn Gilmore <Gilmorec1@livemail.uthscsa.edu>
2021-11-30 11:59:04 -0600

    11/30 class, imputation

--- Selecting_Models.R
+++ Selecting_Models.R
@@ -25,15 +25,17 @@
 # Intro ----
 debug <- 0;
 knitr::opts_chunk$set(echo=debug>-1, warning=debug>0, message=debug>0);
+projectSeed <- 4576
 library(ggplot2)
 library(dplyr)
 library(GGally)
 library(rio)
 library(pander)
 library(printr)
+library(mice)
 options(max.print=42)
 panderOptions('table.split.table',Inf); panderOptions('table.split.cells',Inf);
-
+OutcomeVariable <- "Outcome"
 # Multy Variant Data Sets ----
 #' Dat1
 #'
@@ -41,25 +43,36 @@ dat1 <- import('https://www.ahrq.gov/sites/default/files/wysiwyg/sdohchallenge/d
 head(dat1) %>% pander()
 outcomes <- import("data/SIM_ZCTA_outcomes.csv")
 Zipdat <- left_join(dat1, outcomes)
-Gotcha <- sapply(Zipdat, function(xx){na.omit(xx)%>%unique%>%length()})>1
-Zipdat <- Zipdat[,Gotcha]
+# Gotcha <- sapply(Zipdat, function(xx){na.omit(xx)%>%unique%>%length()})>1
+# Zipdat <- Zipdat[,Gotcha]
+set.seed(projectSeed)
 Zipdat$sample <- sample(c("Train", "Test"), nrow(Zipdat), replace = TRUE, prob = c(0.05, 0.95))
-Ziptrain <- subset(Zipdat, sample=="Train") %>% select(-c("sample", "REGION", "STATE"))
+Ziptrain <- subset(Zipdat, sample=="Train") 
 Gotcha2 <- sapply(Ziptrain, function(xx){na.omit(xx)%>%unique%>%length()})>1
-names(Gotcha2)[!Gotcha2]
-Gotcha3 <- !sapply(Ziptrain, is.numeric)
-names(Gotcha3)[Gotcha3] 
-select(Ziptrain, c("STATE", "REGION")) %>% table()
+names(Gotcha2)[Gotcha2]
+Gotcha3 <- sapply(Ziptrain, is.numeric)
+names(Gotcha3)[Gotcha3]
+Gotcha4 <- sapply(Ziptrain, function(xx){sum(is.na(xx))})<50
+GotAll <- union(names(Gotcha2)[!Gotcha2], names(Gotcha3)[!Gotcha3]) %>% union(names(Gotcha4)[!Gotcha4]) %>% 
+  c(OutcomeVariable, "sample", "REGION", "STATE") %>% setdiff(names(Ziptrain), .)
+FullForm <- paste(GotAll, collapse = "+ ") %>% paste(OutcomeVariable, "~", .)
+MinimalForm <- paste(OutcomeVariable, "~ERS_RUCA1_2010+ ACS_TOTAL_POP_WT")
+# select(Ziptrain, c("STATE", "REGION")) %>% table()
 table(Gotcha3) 
 #+ TrainTest
 # unique(Ziptrain$sample)
 # table(Ziptrain$sample)
 # table(Zipdat$sample)
-
+na.omit(Ziptrain[, GotAll]) %>% dim()
 #' Fitting the intial linear model
 #+ Lm
-POPDat <- lm("Outcome~ACS_TOTAL_POP_WT + CEN_POPDENSITY_2010 + CEN_AREALAND_SQM", Ziptrain)
+POPDat <- update(lm(MinimalForm, Ziptrain), .~.)
 summary(POPDat)%>% pander
 #+ Step
-POPall <- lm(Outcome~., Ziptrain)
-POPStep <- step(POPDat, scope = list(lower = ~1, upper = POPall), k = log(nrow(Ziptrain)))
+POPall <- lm(FullForm, Ziptrain)
+POPStep <- step(POPDat, scope = list(lower = .~1, upper = POPall), direction = "forward", trace = 3, k = 2)
+anova(POPDat, POPall)
+
+ZiptrainImp <- mice(Ziptrain[, c(GotAll, OutcomeVariable)])
+match("ACS_PCT_LIMIT_ENGLISH", GotAll)
+GotAll[32]
